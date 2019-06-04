71721abdaeaa99407aed01e3afb6f9e42309433a
Aitor Jara <aitorjara99@gmail.com>
2019-06-04 20:46:04 +0200

    small code changes

--- Data Processing/store_data_labels.py
+++ Data Processing/store_data_labels.py
@@ -6,12 +6,12 @@ import numpy as np
 
 format = input("write desired output format, for instance: '.jpg'")
 if len(format) < 1 : format = '.png'
-user=input("Write your username")
+user = input("Write your username")
 
 #Define folders and variable patient
 #These paths are just example paths
-read_path ='/Users/'+user+'/Desktop/Task01_BrainTumour/labelsTr/'
-write_path ='/Users/'+user+'/Desktop/CleanSlices/labelsTr/'
+read_path ='/Users/'+ user +'/Desktop/Task01_BrainTumour/labelsTr/'
+write_path ='/Users/'+ user +'/Desktop/CleanSlices/labelsTr/'
 patient = 0
 
 for filename in os.listdir(read_path):
@@ -20,7 +20,7 @@ for filename in os.listdir(read_path):
         data = nii_image.get_data()
         patient += 1
         for slices in range(data.shape[2]):
-            slice_name = 'P' + str(patient + 1) + '_' + str(slices + 1) + format
+            slice_name = 'P' + str(patient) + '_' + str(slices + 1) + format
             slice_path = write_path + slice_name
             data_color = (((data[:,:,slices] - data[:,:,slices].min()) / (data[:,:,slices].max() - data[:,:,slices].min())) * 255.9).astype(np.uint8)
             Image.fromarray(data_color).save(slice_path)
--- Data Processing/store_data_test.py
+++ Data Processing/store_data_test.py
@@ -6,12 +6,12 @@ import numpy as np
 
 format = input("write desired output format, for instance: '.jpg'")
 if len(format) < 1 : format = '.png'
-
+user = input("Write your username")
 
 #Define folders and variable patient
 #These paths are just example paths
-read_path ='/Users/aitorjara/Desktop/Task01_BrainTumour/imagesTs/'
-write_path ='/Users/aitorjara/Desktop/CleanSlices/imagesTs/'
+read_path ='/Users/+ user +/Desktop/Task01_BrainTumour/imagesTs/'
+write_path ='/Users/+ user +/Desktop/CleanSlices/imagesTs/'
 patient = 0
 
 #Iterate through the items in the folder, read data, get data, slice it, decide a name convention and save it
@@ -22,7 +22,7 @@ for filename in os.listdir(read_path):
         patient += 1
         for contrast in range(data.shape[3]):
             for slices in range(data.shape[2]):
-                slice_name = 'P' + str(patient + 1) + 'C' + str(contrast + 1) + '_' + str(slices + 1) + format
+                slice_name = 'P' + str(patient) + 'C' + str(contrast + 1) + '_' + str(slices + 1) + format
                 slice_path = write_path + slice_name
                 data_gray = (((data[:,:,slices,contrast] - data[:,:,slices,contrast].min()) / (data[:,:,slices,contrast].max() - data[:,:,slices,contrast].min())) * 255.9).astype(np.uint8)
                 Image.fromarray(data_gray).save(slice_path)
