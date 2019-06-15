d4efc5a4a046992368ec334f023c9b83160995ab
Aitor Jara <aitorjara99@gmail.com>
2019-06-15 13:03:49 +0200

    Minor changes

--- GenerarImatges.py
+++ GenerarImatges.py
@@ -14,7 +14,7 @@ import pandas as pd
 imagePathTR = "/Users/aitorjara/Desktop/Task01_BrainTumour/imagesTr/"
 labelPathTR = "/Users/aitorjara/Desktop/Task01_BrainTumour/labelsTr/"
 imagePathTS = "/Users/aitorjara/Desktop/Task01_BrainTumour/imagesTs/"
-outputDir = "/Users/aitorjara/Desktop/CleanSlices/Slices_David"
+outputDir = "/Users/aitorjara/Desktop/CleanSlices/Slices_David/"
 
 
 outputFileType = "jpg" # "jpg", "png"
