bd50f063b3fe2ff3e3ab56cc579d9925382612a0
Aitor Jara <aitorjara99@gmail.com>
2019-06-15 13:23:20 +0200

    changes train model

--- model.py
+++ model.py
@@ -1,35 +1,21 @@
 #https://github.com/zhixuhao/unet/blob/master/model.py
 
 import tensorflow as tf
-import utils
-import os
-
-def est_unet():
-    #Keras model to TF estimator
-    model_dir = os.path.join("/home/deivit/Desktop/dades/Documents/david/upc/AIDL/projecte/unet", "models/unet")
-    utils.ensure_dir(model_dir)
-    os.makedirs(model_dir, exist_ok=True)
-    print("model_dir: ", model_dir)
-    est_unet = tf.keras.estimator.model_to_estimator(keras_model=unet(),
-                                                         model_dir=model_dir)
-    return est_unet
-
 
 
 def unet(inputs, training=False):
 
-    #pretrained_weights = None,input_size = (240,240,4)):
-    # inputs = tf.keras.Input(input_size)
-    #inputs = tf.keras.layers.Input(input_size)
     conv1 = tf.keras.layers.Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(inputs)
-    conv1 = tf.keras.layers.Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv1)
     pool1 = tf.keras.layers.MaxPooling2D(pool_size=(2, 2))(conv1)
+
     conv2 = tf.keras.layers.Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool1)
     conv2 = tf.keras.layers.Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv2)
     pool2 = tf.keras.layers.MaxPooling2D(pool_size=(2, 2))(conv2)
+
     conv3 = tf.keras.layers.Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool2)
     conv3 = tf.keras.layers.Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv3)
     pool3 = tf.keras.layers.MaxPooling2D(pool_size=(2, 2))(conv3)
+
     conv4 = tf.keras.layers.Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool3)
     conv4 = tf.keras.layers.Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv4)
     drop4 = tf.keras.layers.Dropout(0.5)(conv4)
@@ -60,32 +46,4 @@ def unet(inputs, training=False):
     conv9 = tf.keras.layers.Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
     conv9 = tf.keras.layers.Conv2D(2, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
     conv10 = tf.keras.layers.Conv2D(1, 1, activation = 'sigmoid')(conv9)
-
     return conv10
-
-    #model = tf.keras.Model(inputs = inputs, outputs = conv10)
-
-    #print(model.summary())
-    #for layer in model.layers:
-    #    print(layer.name, layer.input.shape, layer.output.shape)
-
-
-    #model.compile(optimizer = tf.keras.optimizers.Adam(lr = 1e-4), loss = 'binary_crossentropy', metrics = ['accuracy'])
-    #model.compile(optimizer = 'adam', loss = 'sparse_categorical_crossentropy')
-
-    #model.compile(
-    #            optimizer=keras.optimizers.Adadelta(),
-    #            loss='sparse_categorical_crossentropy',
-    #            metrics=['sparse_categorical_accuracy'])
-    run_opts = tf.RunOptions(report_tensor_allocations_upon_oom = True)
-    model.compile(
-            optimizer='adam',
-            loss='sparse_categorical_crossentropy',
-            metrics=['sparse_categorical_accuracy'])
-            #options = [run_opts]
-    #model.summary()
-
-    if(pretrained_weights):
-        model.load_weights(pretrained_weights)
-
-    return model
---
+++ rutas.py
@@ -0,0 +1,11 @@
+rutas
+#GenerateTFRecords:
+imagePathTR = "/Users/aitorjara/Desktop/Task01_BrainTumour/imagesTr/"
+labelPathTR = "/Users/aitorjara/Desktop/Task01_BrainTumour/labelsTr/"
+imagePathTS = "/Users/aitorjara/Desktop/Task01_BrainTumour/imagesTs/"
+outputDir = "/Users/aitorjara/Desktop/CleanSlices/Slices_David/"
+#GenerarImatges
+outputDir = "/Users/aitorjara/Desktop/CleanSlices/TFRecords/"
+#ExtractTFRecords
+tfrecord_file='/Users/aitorjara/Desktop/CleanSlices/TFRecords/'
+outputFolder='/Users/aitorjara/Desktop/CleanSlices/ExtractTFRecords/'
