5460b7ef1ea46e265ff2ab56adb525ca21e6635d
endinet <git@endinet.net>
2019-06-19 20:45:09 +0200

    Santi forever

--- model.py
+++ model.py
@@ -45,5 +45,14 @@ def unet(inputs, training=False):
     conv9 = tf.keras.layers.Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge9)
     conv9 = tf.keras.layers.Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
     conv9 = tf.keras.layers.Conv2D(2, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
-    conv10 = tf.keras.layers.Conv2D(1, 1, activation = 'sigmoid')(conv9)
+    
+    conv10 = tf.keras.layers.Conv2D(4, 1)(conv9)
+    #conv10 = tf.keras.layers.Conv2D(filters=4, kernel_size=(1, 1))(conv9)
+    #conv10 = tf.keras.layers.BatchNormalization()(conv10)
+    #conv10 = tf.keras.layers.Reshape((192*192, 4), input_shape=(192, 192, 4))(conv10)
+    #conv10 = tf.keras.layers.Activation('softmax')(conv10)    
+    
+    #conv10 = tf.keras.layers.Conv2D(4, 1, activation = 'softmax')(conv9)
+    
+    print(" * Output: %s" % conv10)
     return conv10
--- train.py
+++ train.py
@@ -1,164 +1,198 @@
-#https://www.dlology.com/blog/an-easy-guide-to-build-new-tensorflow-datasets-and-estimator-with-keras-model/
-#https://www.dlology.com/blog/how-to-leverage-tensorflows-tfrecord-to-train-keras-model/
-#http://androidkt.com/train-keras-model-with-tensorflow-estimators-and-datasets-api/
-#https://github.com/Tony607/Keras_catVSdog_tf_estimator/blob/master/keras_estimator_vgg16-cat_vs_dog.ipynb
-#http://androidkt.com/feeding-your-own-data-set-into-the-cnn-model-in-tensorflow/
-
-
-#data augmentation
-#https://colab.research.google.com/github/tensorflow/models/blob/master/samples/outreach/blogs/segmentation_blogpost/image_segmentation.ipynb#scrollTo=tkNqQaR2HQbd
-#http://androidkt.com/tensorflow-image-augmentation-using-tf-image/
-
-#watch -n0.1 nvidia-smi
-
-import tensorflow as tf
 import os
-import utils
-from tensorflow.keras import backend
-#from PIL import Image
 import argparse
-#from tensorflow.keras.preprocessing import image
-import dataset
+import tensorflow as tf
+from dataset import *
+import utils
+from tensorflow.python.keras import backend as K
 
-
-
-NUM_EPOCHS = 3
-BATCH_SIZE= 1
-LEARNING_RATE= 1e-4
-STEPS_SAVER = 3
-
-#"zhixuhao"
-#"nuria"
-# MODEL_TO_USE="nuria"
-
-def get_file_lists(data_dir):
-    import glob
-    train_list = glob.glob(data_dir + '/Training/' + '*')
-    valid_list = glob.glob(data_dir + '/Validation/' + '*')
-    test_list = glob.glob(data_dir + '/Testing/' + '*')
-    if len(train_list) == 0 and \
-                    len(valid_list) == 0:
-        raise IOError('No files found at specified path!')
-    return train_list, valid_list, test_list
-
-def train_input_fn(file_path,num_epochs, batch_size):
-    return dataset.input_fn(file_path,True,num_epochs,batch_size)
-
-def validation_input_fn(file_path,num_epochs, batch_size):
-    return dataset.input_fn(file_path,False,num_epochs,batch_size)
-
-def test_input_fn(file_path,num_epochs, batch_size):
-    return dataset.input_fn(file_path,False,num_epochs,batch_size)
-
-#logits: predicció
-def loss(labels, logits):
-    labels = backend.print_tensor(labels, message='labels = ')
-    logits = backend.print_tensor(logits, message='logits = ')
-
-    return tf.losses.sparse_softmax_cross_entropy(labels, logits)
-    #return tf.nn.softmax_cross_entropy_with_logits_v2(logits=logits, labels=labels)
 
     
-def main(trainingdir, model, num_epochs, batch_size, learning_rate, logdir, restore_weights):    
-    train_list,valid_list, test_list = get_file_lists(trainingdir)
-    next_batch = train_input_fn(train_list,num_epochs, batch_size)
-    test_batch = test_input_fn(test_list,num_epochs, batch_size)
-    # tf Graph Input
-    x = tf.placeholder(tf.float32, shape=[None, 192, 192, 4], name='x')
-    y = tf.placeholder(tf.int32, shape=[None, 192, 192, 1], name='y')
+#########################################################
+    
+    #Training parameters
+NUM_EPOCHS = 1
+BATCH_SIZE= 25
+LEARNING_RATE= 1e-4
+STEPS_SAVER =100
+MODEL_TO_USE="zhixuhao"
+TRAININGDIR="../BrainTumourImages/Generated/"
+LOGDIR='/tmp/aidl'
+
+#########################################################
+
+#number of training and validation data
+
+#function that returns the number of records in a set of TFRecords stored a directory
+def count_records(path):
+    num = 0
+    for record_file in os.listdir(path):
+        TFRecord_path = os.path.join(path,record_file)
+        for record in tf.io.tf_record_iterator(TFRecord_path):
+            num += 1
+    return num
+
+def dice_coeff(y_true, y_pred):
+    smooth = 1.
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    score = (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+    return score
+
+def dice_loss(y_true, y_pred):
+    loss = 1 - dice_coeff(y_true, y_pred)
+    return loss
+    
+def dice_coef_multilabel(y_true, y_pred, numLabels=5):
+    dice=0
+    for index in range(numLabels):
+        dice -= dice_coeff(y_true[:,index,:,:,:], y_pred[:,index,:,:,:])
+    return dice  
+
+def loss_rara1(labels, logits):
+    #https://github.com/perslev/MultiPlanarUNet/
+    # Flatten
+    labels = tf.cast(tf.reshape(labels, [-1]), tf.int32)
+    logits = tf.reshape(logits, [-1, 4])
+
+    # Calculate in-batch class counts and total counts
+    target_one_hot = tf.one_hot(labels, 4)
+    counts = tf.cast(tf.reduce_sum(target_one_hot, axis=0), tf.float32)
+    total_counts = tf.reduce_sum(counts)
+
+    # Compute balanced sample weights
+    weights = (tf.ones_like(counts) * total_counts) / (counts * 4)
+
+    # Compute sample weights from class weights
+    weights = tf.gather(weights, labels)
+
+    return tf.losses.sparse_softmax_cross_entropy(labels, logits, weights) 
+
+def loss_rara2(labels, logits):
+    #https://github.com/tensorflow/tensorflow/issues/10021
+    #https://stackoverflow.com/questions/40198364/how-can-i-implement-a-weighted-cross-entropy-loss-in-tensorflow-using-sparse-sof/46984951#46984951
+    class_weights = tf.constant([0.1 , 0.3 , 0.3 , 0.3])  # 3 classes
+    sample_weights = tf.gather(class_weights, labels)
+    return tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits,weights=sample_weights)
+
+def loss_sparse(labels, logits):
+    #labels = backend.print_tensor(labels, message='labels = ')
+    #logits = backend.print_tensor(logits, message='logits = ')  
+    return tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
+    #return tf.nn.sparse_softmax_cross_entropy_with_logits(labels=labels, logits=logits)
+    #return tf.nn.softmax_cross_entropy_with_logits_v2(logits=logits, labels=labels)
+    
+ 
 
 
-    if model == "zhixuhao":
+def main(trainingdir, model, num_epochs, batch_size, learning_rate, logdir, restore_weights): 
+
+    
+    global_step=tf.get_variable('global_step',dtype=tf.int32,initializer=0,trainable=False)    
+    #training
+    train_images = count_records(os.path.join(trainingdir, 'Training'))
+    #validation
+    valid_images = count_records(os.path.join(trainingdir, 'Validation'))
+
+    size_batch_valid = int(valid_images/int(train_images/batch_size))
+    
+    ######################################## DATAFLOW GRAPH #########################################################
+    
+    train_list, valid_list, test_list = get_file_lists(trainingdir)
+    batch_train = input_fn(filenames=train_list,mode="training", num_epochs=num_epochs, batch_size=batch_size)
+    batch_valid = input_fn(filenames=valid_list, mode='validation', num_epochs=num_epochs, batch_size=size_batch_valid)
+    batch_test = input_fn(filenames=test_list, mode='testing', num_epochs=num_epochs, batch_size=size_batch_valid)
+        
+    # Graph inputs
+    x = tf.placeholder('float', shape=[None, 192, 192, 4], name='x')
+    y = tf.placeholder('int32', shape=[None, 192, 192, 1], name='y')
+
+    tf.summary.image("input_0",tf.expand_dims(x[:,:,:,0],axis=-1))
+    tf.summary.image("labels",tf.cast(y,tf.float32))
+    #y=tf.squeeze(y)
+    
+    
+    if args.model == "zhixuhao":
         import model as model
-        y_ = model.unet(x,True)
-    elif model == "nuria":
-        import unet
-        y_ = unet.unet_model(x, training=True)
-
-
-    loss = tf.reduce_mean(loss(y, y_))
-    #loss = tf.losses.get_total_loss()
-    #loss = tf.reduce_mean(loss(labels=y,logits=y_))
-
-    optimizer = tf.train.AdamOptimizer(learning_rate=LEARNING_RATE)
-    #optimizer = tf.train.AdadeltaOptimizer(learning_rate=LEARNING_RATE)
-    #optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.05)
-
-    train_op = optimizer.minimize(loss)
-
+        logits = model.unet(x,True)
+    elif args.model == "nuria":
+        import unet         
+        logits = unet.unet_model(x, training=True, norm_option=True)        
+        
+    tf.summary.image("prediction", logits[:,:,:,1:])
+    tf.summary.histogram("logits",logits)
+    #Forward and backprop pass
+    loss= tf.reduce_mean(loss_sparse(labels=y, logits=logits))
+    tf.summary.scalar("loss", loss)
+    
+    optimizer = tf.train.AdamOptimizer(learning_rate)
+    train_op = optimizer.minimize(loss,global_step=global_step)
     
     # Weight saver
-    model_checkpoint_path = os.path.join(logdir, 'BrainScan')
+    model_checkpoint_path = os.path.join(logdir, 'Checkpoint')
     saver = tf.train.Saver()
     
+    ######################################## RUN SESSION #########################################################
+    summary_op=tf.summary.merge_all()
     
-    #prediction = tf.argmax(y_, 1)
     with tf.Session() as sess:
-
+    
+        # Initialize Variables
+        sess.run(tf.global_variables_initializer())
+        sess.run(tf.local_variables_initializer())
+        
+        # op to write logs to Tensorboard
+        logdir = os.path.expanduser(args.logdir)
+        utils.ensure_dir(logdir)
+        writer = tf.summary.FileWriter(logdir, graph=tf.get_default_graph())         
+        
         if restore_weights:
             saver.restore(sess, restore_weights)
         else:
-            sess.run(tf.global_variables_initializer())
-        
-        # train
-        # sess.run(tf.global_variables_initializer())
-
-        # op to write logs to Tensorboard
-        logdir = os.path.expanduser(logdir)
-        utils.ensure_dir(logdir)
-        writer = tf.summary.FileWriter(logdir, graph=tf.get_default_graph())
-
-
+            sess.run(tf.global_variables_initializer())       
+    
+        #training, validation and saving
         for epoch in range(num_epochs):
-        
-        
-            x_train_batch, y_train_batch = sess.run(next_batch)
-            current_loss, _ = sess.run([loss, train_op], feed_dict={x:x_train_batch,
-                                                                     y:y_train_batch})
-            print(current_loss)
-            
-            if epoch % STEPS_SAVER == 0:
-                print('Step {}\tSaving weights to {}'.format(model_checkpoint_path))
-                saver.save(sess, save_path=model_checkpoint_path)
-            
-            
-
+            for step in range(int(train_images/batch_size)):
+    
+                #training
+                batch_images, batch_labels = sess.run(batch_train)
+                _,cost,summary_val,step,logits_val = sess.run([train_op,loss,summary_op,global_step,logits], feed_dict={x:batch_images, y:batch_labels})
+                writer.add_summary(summary_val,step)
+                #validation
+                batch_images_valid, batch_labels_valid = sess.run(batch_valid)
+                cost_valid = sess.run(loss, feed_dict={x:batch_images_valid, y:batch_labels_valid})
+    
+                print('\nEpoch {}, batch {} -- Loss: {:.3f}, Validation Loss: {:.3f}'.format(epoch+1, step+1, cost, cost_valid))
+                
+                if step % STEPS_SAVER == 0:
+                    print('Step {}\tSaving weights to {}'.format(step+1, model_checkpoint_path))
+                    saver.save(sess, save_path=model_checkpoint_path,global_step=global_step)
+                    
+    #Predictions
         try:
             y_pred = []
-
-            x_test_batch = sess.run(test_batch)
+            x_test_batch = sess.run(batch_test)
             sess.run([y_pred],feed_dict={x:x_test_batch})
             print(y_pred)
+    
         except tf.errors.OutOfRangeError:
-            print("u fucked up")
+            print("u fucked up")                    
+            
 
-        """
-        #Les dades son les correctes:
-            for j in range(x_train_batch.shape[0]):
-                outputFile=os.path.join("/home/deivit/Desktop/dades/Documents/david/upc/AIDL/projecte/unet/BrainTumourImages/caca",str(epoch) + str(j)  +  ".jpg" )
-                data=x_train_batch[j,:,:,0]
-                #data = data.reshape(192, 192, 4)
-                im=Image.fromarray(data)
-                im.save(outputFile)
-            """
-    
-    
+
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Pipeline execution')
-    parser.add_argument('-t', '--trainingdir', default='../BrainTumourImages/Generated/', help='Location of the TFRecors for training')
-    parser.add_argument('-l', '--logdir', default='/tmp/aidl', help='Log dir for tfevents')
+    parser.add_argument('-t', '--trainingdir', default=TRAININGDIR, help='Location of the TFRecors for training')
+    parser.add_argument('-l', '--logdir', default=LOGDIR, help='Log dir for tfevents')
     parser.add_argument('-e', '--num_epochs', type=int, default=NUM_EPOCHS, help='Number of epochs')
     parser.add_argument('-b', '--batch_size', type=int, default=BATCH_SIZE, help='Batch size')
-    parser.add_argument('-lr', '--learning_rate', type=float, default=1e-5, help='Learning rate')
+    parser.add_argument('-lr', '--learning_rate', type=float, default=LEARNING_RATE, help='Learning rate')
     parser.add_argument('-r', '--restore', help='Path to model checkpoint to restore weights from.')
-    parser.add_argument('-m', '--model', default="nuria",help='Model to use, either zhixuhao or nuria.')
+    parser.add_argument('-m', '--model', default=MODEL_TO_USE,help='Model to use, either zhixuhao or nuria.')
     
     args = parser.parse_args()
     
     
-    main(args.trainingdir,args.model, args.num_epochs, args.batch_size, args.learning_rate, args.logdir, args.restore)
-
-    
-    
-    
+main(args.trainingdir,args.model, args.num_epochs, args.batch_size, args.learning_rate, args.logdir, args.restore)            
