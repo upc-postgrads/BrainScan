b536fa0a13eeabc4bde972835ca15820882679bd
EddieMG <32465773+EddieMG@users.noreply.github.com>
2019-06-19 12:14:35 +0200

    Added Saver and minor changes
    Moved to main function, eliminated the parsing on the main fucntion.

--- train.py
+++ train.py
@@ -25,10 +25,11 @@ import dataset
 NUM_EPOCHS = 3
 BATCH_SIZE= 1
 LEARNING_RATE= 1e-4
+STEPS_SAVER = 3
 
 #"zhixuhao"
 #"nuria"
-MODEL_TO_USE="nuria"
+# MODEL_TO_USE="nuria"
 
 def get_file_lists(data_dir):
     import glob
@@ -57,28 +58,20 @@ def loss(labels, logits):
     return tf.losses.sparse_softmax_cross_entropy(labels, logits)
     #return tf.nn.softmax_cross_entropy_with_logits_v2(logits=logits, labels=labels)
 
-
-if __name__ == '__main__':
-
-    parser = argparse.ArgumentParser(description='Pipeline execution')
-    parser.add_argument('-t', '--trainingdir', default='../BrainTumourImages/Generated/', help='Location of the TFRecors for training')
-    parser.add_argument('-l', '--logdir', default='/tmp/aidl', help='Log dir for tfevents')
-    parser.add_argument('-e', '--num_epochs', type=int, default=NUM_EPOCHS, help='Number of epochs')
-    parser.add_argument('-b', '--batch_size', type=int, default=BATCH_SIZE, help='Batch size')
-
-    args = parser.parse_args()
-    train_list,valid_list, test_list = get_file_lists(args.trainingdir)
-    next_batch = train_input_fn(train_list,args.num_epochs, args.batch_size)
-    test_batch = test_input_fn(test_list,args.num_epochs, args.batch_size)
+    
+def main(trainingdir, model, num_epochs, batch_size, learning_rate, logdir, restore_weights):    
+    train_list,valid_list, test_list = get_file_lists(trainingdir)
+    next_batch = train_input_fn(train_list,num_epochs, batch_size)
+    test_batch = test_input_fn(test_list,num_epochs, batch_size)
     # tf Graph Input
     x = tf.placeholder(tf.float32, shape=[None, 192, 192, 4], name='x')
     y = tf.placeholder(tf.int32, shape=[None, 192, 192, 1], name='y')
 
 
-    if MODEL_TO_USE == "zhixuhao":
+    if model == "zhixuhao":
         import model as model
         y_ = model.unet(x,True)
-    elif MODEL_TO_USE == "nuria":
+    elif model == "nuria":
         import unet
         y_ = unet.unet_model(x, training=True)
 
@@ -93,23 +86,42 @@ if __name__ == '__main__':
 
     train_op = optimizer.minimize(loss)
 
+    
+    # Weight saver
+    model_checkpoint_path = os.path.join(logdir, 'BrainScan')
+    saver = tf.train.Saver()
+    
+    
     #prediction = tf.argmax(y_, 1)
     with tf.Session() as sess:
 
+        if restore_weights:
+            saver.restore(sess, restore_weights)
+        else:
+            sess.run(tf.global_variables_initializer())
+        
         # train
-        sess.run(tf.global_variables_initializer())
+        # sess.run(tf.global_variables_initializer())
 
         # op to write logs to Tensorboard
-        logdir = os.path.expanduser(args.logdir)
+        logdir = os.path.expanduser(logdir)
         utils.ensure_dir(logdir)
         writer = tf.summary.FileWriter(logdir, graph=tf.get_default_graph())
 
 
-        for epoch in range(args.num_epochs):
+        for epoch in range(num_epochs):
+        
+        
             x_train_batch, y_train_batch = sess.run(next_batch)
             current_loss, _ = sess.run([loss, train_op], feed_dict={x:x_train_batch,
                                                                      y:y_train_batch})
             print(current_loss)
+            
+            if epoch % STEPS_SAVER == 0:
+                print('Step {}\tSaving weights to {}'.format(step, model_checkpoint_path))
+                saver.save(sess, save_path=model_checkpoint_path)
+            
+            
 
         try:
             y_pred = []
@@ -129,3 +141,24 @@ if __name__ == '__main__':
                 im=Image.fromarray(data)
                 im.save(outputFile)
             """
+    
+    
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Pipeline execution')
+    parser.add_argument('-t', '--trainingdir', default='../BrainTumourImages/Generated/', help='Location of the TFRecors for training')
+    parser.add_argument('-l', '--logdir', default='/tmp/aidl', help='Log dir for tfevents')
+    parser.add_argument('-e', '--num_epochs', type=int, default=NUM_EPOCHS, help='Number of epochs')
+    parser.add_argument('-b', '--batch_size', type=int, default=BATCH_SIZE, help='Batch size')
+    parser.add_argument('-lr', '--learning_rate', type=float, default=1e-5, help='Learning rate')
+    parser.add_argument('-r', '--restore', help='Path to model checkpoint to restore weights from.')
+    parser.add_argument('-m', '--model', default="nuria",help='Model to use, either zhixuhao or nuria.')
+    
+    args = parser.parse_args()
+    
+    
+    main(args.trainingdir,args.model, args.num_epochs, args.batch_size, args.learning_rate, args.logdir, args.restore)
+
+    
+    
+    
