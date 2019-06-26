e081e80b34c2750e514f4d385587ef9f7bd1a2ec
endinet <git@endinet.net>
2019-06-26 20:27:54 +0200

    Add files via upload

--- src/models/unet_keras.py
+++ src/models/unet_keras.py
@@ -3,7 +3,7 @@
 import tensorflow as tf
 
 
-def unet(inputs, training=False):
+def unet(inputs, training=False,binarize_labels=False):
 
     conv1 = tf.keras.layers.Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(inputs)
     pool1 = tf.keras.layers.MaxPooling2D(pool_size=(2, 2))(conv1)
@@ -46,5 +46,9 @@ def unet(inputs, training=False):
     conv9 = tf.keras.layers.Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
     conv9 = tf.keras.layers.Conv2D(2, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
 
-    conv10 = tf.keras.layers.Conv2D(4, 1)(conv9)
+    if binarize_labels:
+        conv10 = tf.keras.layers.Conv2D(2, 1)(conv9)
+    else:
+        conv10 = tf.keras.layers.Conv2D(4, 1)(conv9)
+
     return conv10
