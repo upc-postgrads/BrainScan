2a906e1206e76e6333d90df3f6604b8691907450
endinet <git@endinet.net>
2019-06-26 20:27:05 +0200

    Add files via upload

--- src/dataset.py
+++ src/dataset.py
@@ -18,7 +18,7 @@ def get_file_lists(data_dir):
 
 
 
-def create_dataset(filenames, mode, num_epochs=1, batch_size=1,perform_shuffle=False,perform_one_hot=True):
+def create_dataset(filenames, mode, num_epochs=1, batch_size=1,perform_shuffle=False,perform_one_hot=True,binarize_labels=False):
     if mode == 'validation':
         perform_shuffle = False
     if mode == 'training':
@@ -52,17 +52,26 @@ def create_dataset(filenames, mode, num_epochs=1, batch_size=1,perform_shuffle=F
 
         frames = tf.reshape(frames,(height, width, depth))
         label = tf.reshape(label,(height, width,-1))
-        tf.expand_dims(label, 1).shape
+
         
+        # Binarize label to only have value 0 or 1 (label > 0)
+        binary_labels = tf.cast(label > 0, dtype=tf.int32)
        
         #Add data augmentation here
         frames=tf.image.central_crop(frames,0.8)
         label=tf.image.central_crop(label,0.8)
+        if binarize_labels:
+            binary_labels=tf.image.central_crop(binary_labels,0.8)
         
         if perform_one_hot:
             label=tf.one_hot(indices=tf.squeeze(label), depth=4)
+            if binarize_labels:
+                binary_labels=tf.one_hot(indices=tf.squeeze(binary_labels), depth=2)
         
-        return frames, label
+        if binarize_labels:
+            return frames, binary_labels
+        else:
+            return frames, label
 
 
 
--- src/train.py
+++ src/train.py
@@ -26,18 +26,20 @@ RESTORE_WEIGHTS=False
 TRAININGDIR = "/path_of_training_images"
 LOGDIR = '/tmp'
 PERFORM_ONE_HOT=True
+BINARIZE_LABELS=True
 
-def main(trainingdir, model, num_epochs, size_batch_train, size_batch_test, size_batch_valid, step_metrics, steps_saver, learning_rate, logdir, restore_weights,perform_one_hot):
+
+def main(trainingdir, model, num_epochs, size_batch_train, size_batch_test, size_batch_valid, step_metrics, steps_saver, learning_rate, logdir, restore_weights,perform_one_hot,binarize_labels):
 
     global_step=tf.get_variable('global_step',dtype=tf.int32,initializer=0,trainable=False)
 
     train_list, valid_list, test_list = get_file_lists(trainingdir)
   
-    train_dataset = create_dataset(filenames=train_list,mode="training", num_epochs=num_epochs, batch_size=size_batch_train,perform_one_hot=perform_one_hot)
+    train_dataset = create_dataset(filenames=train_list,mode="training", num_epochs=1, batch_size=size_batch_train,perform_one_hot=perform_one_hot,binarize_labels=binarize_labels)
     train_iterator = train_dataset.make_initializable_iterator()
-    validation_dataset = create_dataset(filenames=valid_list,mode="validation", num_epochs=1, batch_size=size_batch_valid,perform_one_hot=perform_one_hot)
+    validation_dataset = create_dataset(filenames=valid_list,mode="validation", num_epochs=1, batch_size=size_batch_valid,perform_one_hot=perform_one_hot,binarize_labels=binarize_labels)
     validation_iterator = validation_dataset.make_initializable_iterator() 
-    test_dataset = create_dataset(filenames=test_list,mode="testing", num_epochs=1, batch_size=size_batch_test,perform_one_hot=perform_one_hot)      
+    test_dataset = create_dataset(filenames=test_list,mode="testing", num_epochs=1, batch_size=size_batch_test,perform_one_hot=perform_one_hot,binarize_labels=binarize_labels)      
     test_iterator = test_dataset.make_initializable_iterator() 
     
     # Feedable iterator assigns each iterator a unique string handle it is going to work on 
@@ -45,35 +47,41 @@ def main(trainingdir, model, num_epochs, size_batch_train, size_batch_test, size
     iterator = tf.data.Iterator.from_string_handle(handle, train_dataset.output_types, train_dataset.output_shapes)
     x, y = iterator.get_next()
 
+    x.set_shape([None, 192, 192, 4])
+    x = tf.cast(x, tf.float32)
+    
+    training_placeholder = tf.placeholder(dtype=tf.bool, shape=[], name='training_placeholder')      
+
     if model == "unet_keras":
         from models import unet_keras as model
-        x.set_shape([None, 192, 192, 4])
-        x = tf.cast(x, tf.float32)        
-        logits = model.unet(x,True)
+        logits = model.unet(x,training_placeholder,binarize_labels=binarize_labels)
     elif model == "unet_tensorflow":
         from models import unet_tensorflow as model
-        logits = model.unet(x, training=True, norm_option=True)
+        logits = model.unet(x, training=training_placeholder, norm_option=True,binarize_labels=binarize_labels)
     
     if perform_one_hot:
     #Forward and backprop pass
-        y.set_shape([None, 192, 192, 4])
+        if binarize_labels:
+            y.set_shape([None, 192, 192, 2])
+        else:
+            y.set_shape([None, 192, 192, 4])
     else:
         y.set_shape([None, 192, 192, 1])        
         
     y = tf.cast(y, tf.int32)
     
     if perform_one_hot:
-        loss_op = tf.reduce_mean(tf.losses.softmax_cross_entropy(onehot_labels=y, logits=logits))
+        tf.losses.softmax_cross_entropy(onehot_labels=y, logits=logits)
+        loss_op = tf.losses.get_total_loss(name='loss_op')
         IoU_metrics = tf.metrics.mean_iou(labels=y, predictions=logits, num_classes=4)        
     else:
-        loss_op= tf.reduce_mean(tf.losses.sparse_softmax_cross_entropy(labels=y, logits=logits))   
+        tf.losses.sparse_softmax_cross_entropy(labels=y, logits=logits)
+        loss_op = tf.losses.get_total_loss(name='loss_op')
         
         
-
-    #loss_op = tf.losses.get_total_loss()
-
     optimizer = tf.train.AdamOptimizer(learning_rate)
-    train_op = optimizer.minimize(loss_op,global_step=global_step)
+    update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+    train_op = tf.group([update_ops, optimizer.minimize(loss_op,global_step=global_step)])
    
     # Weight saver
     model_checkpoint_path = os.path.join(logdir, 'Checkpoint')
@@ -87,12 +95,14 @@ def main(trainingdir, model, num_epochs, size_batch_train, size_batch_test, size
     if perform_one_hot:
         tf.summary.image('labels_0',tf.expand_dims(tf.cast(y,tf.float32)[:,:,:,0],axis=-1))
         tf.summary.image('labels_1',tf.expand_dims(tf.cast(y,tf.float32)[:,:,:,1],axis=-1))
-        tf.summary.image('labels_2',tf.expand_dims(tf.cast(y,tf.float32)[:,:,:,2],axis=-1)) 
-        tf.summary.image('labels_3',tf.expand_dims(tf.cast(y,tf.float32)[:,:,:,3],axis=-1)) 
+        if not binarize_labels:
+            tf.summary.image('labels_2',tf.expand_dims(tf.cast(y,tf.float32)[:,:,:,2],axis=-1)) 
+            tf.summary.image('labels_3',tf.expand_dims(tf.cast(y,tf.float32)[:,:,:,3],axis=-1)) 
         tf.summary.image('prediction_0',tf.expand_dims(logits[:,:,:,0],axis=-1))
         tf.summary.image('prediction_1',tf.expand_dims(logits[:,:,:,1],axis=-1))
-        tf.summary.image('prediction_2',tf.expand_dims(logits[:,:,:,2],axis=-1)) 
-        tf.summary.image('prediction_3',tf.expand_dims(logits[:,:,:,3],axis=-1))
+        if not binarize_labels:
+            tf.summary.image('prediction_2',tf.expand_dims(logits[:,:,:,2],axis=-1)) 
+            tf.summary.image('prediction_3',tf.expand_dims(logits[:,:,:,3],axis=-1))
     else:
         tf.summary.image("labels",tf.cast(y,tf.float32)) 
         tf.summary.image("prediction", logits[:,:,:,1:])         
@@ -139,12 +149,13 @@ def main(trainingdir, model, num_epochs, size_batch_train, size_batch_test, size
                     step += 1
                     print('\n Training step: Epoch {}, batch {} -- Loss: {:.3f}'.format(epoch+1, step, cost))
                     
-                    
+                    """
                     #validation
                     if step % step_metrics == 0:
                         total_validation_loss = []
                         sess.run(validation_iterator.initializer)
                         step_val=0
+                        print('\n Step {}\tSaving weights to {}'.format(step+1, model_checkpoint_path))
                         try:    
                             while True:
                                 _, cost_valid = sess.run([logits, loss_op], feed_dict={handle: validation_handle})
@@ -156,11 +167,11 @@ def main(trainingdir, model, num_epochs, size_batch_train, size_batch_test, size
                             total_validation_loss = np.mean(total_validation_loss)         
                             validation_loss_summary = tf.Summary(value=[tf.Summary.Value(tag="validation_loss", simple_value=total_validation_loss)])
                             writer.add_summary(validation_loss_summary,step_gl)                        
-                        print('Epoch {} and training batch {} -- Validation loss: {:.3f}'.format(epoch+1, step+1,total_validation_loss))
-                    
+                        print('\n Epoch {} and training batch {} -- Validation loss: {:.3f}'.format(epoch+1, step+1,total_validation_loss))
+                    """
                     #saving    
                     if step % STEPS_SAVER == 0:
-                        print('Step {}\tSaving weights to {}'.format(step+1, model_checkpoint_path))
+                        print('\n Step {}\tSaving weights to {}'.format(step+1, model_checkpoint_path))
                         saver.save(sess, save_path=model_checkpoint_path,global_step=global_step)                             
                                 
             except tf.errors.OutOfRangeError:
@@ -183,6 +194,7 @@ if __name__ == '__main__':
     parser.add_argument('-r', '--restore_weights', type=float, default=RESTORE_WEIGHTS, help='Restore weights from logdir path.')
     parser.add_argument('-m', '--model', default=MODEL_TO_USE,help='Model to use, either unet_keras or unet_tensorflow.')
     parser.add_argument('-oh', '--perform_one_hot', default=PERFORM_ONE_HOT,help='Perform on-hot encoding for labels.')    
+    parser.add_argument('-bi', '--binarize_labels', default=BINARIZE_LABELS,help='Perform binarization for labels.')    
     
 
     args = parser.parse_args()
